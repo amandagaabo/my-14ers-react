9dd6356ad000c9944af235a3161a7ff24ee13b65
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-01-01 21:19:50 -0700

    lint fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App'
+import App from './containers/App';
 import './styles.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 registerServiceWorker();
