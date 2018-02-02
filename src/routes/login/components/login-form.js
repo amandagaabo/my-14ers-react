3001a9c2bb1654171ab28bf52c1d289aed3a9b1e
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-02-02 11:40:51 -0700

    connected login

--- src/routes/login/components/login-form.js
+++ src/routes/login/components/login-form.js
@@ -3,71 +3,71 @@ import { reduxForm, Field, focus } from 'redux-form';
 import { Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Input from '../../app/components/input';
+import { login } from './../../../modules/auth/actions';
 import { required, nonEmpty, email } from '../../../utils/validators';
 
-export class LoginForm extends React.Component {
-  onSubmit(values) {
-    console.log('Submitted with values', values);
+export function LoginForm(props) {
+  function onSubmit(values) {
+    const { email, password } = values;
+    return props.dispatch(login(email, password));
   }
 
-  render() {
-    let errorMessage;
-    if (this.props.error) {
-      errorMessage = (
-        <div className="message message-error">{this.props.error}</div>
-      );
-    }
-
-    return (
-      <form
-        onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}
-      >
-
-        <Field
-          component={Input}
-          name="email"
-          type="email"
-          label="Email"
-          autofocus
-          validate={[required, nonEmpty, email]}
-        />
-
-        <Field
-          component={Input}
-          name="password"
-          id="password"
-          type="password"
-          label="Password"
-          validate={[required, nonEmpty]}
-        />
-
-        {errorMessage}
-
-        <Col xs={12} className="form-button" >
-          <button
-            type="submit"
-            disabled={this.props.pristine || this.props.submitting}
-          >
-            Login
-          </button>
-        </Col>
-      </form>
+  let errorMessage;
+  if (props.error) {
+    errorMessage = (
+      <div className="message message-error">{props.error}</div>
     );
   }
+
+  return (
+    <form
+      onSubmit={props.handleSubmit(values => onSubmit(values))}
+    >
+
+      <Field
+        component={Input}
+        name="email"
+        type="email"
+        label="Email"
+        autofocus
+        validate={[required, nonEmpty, email]}
+      />
+
+      <Field
+        component={Input}
+        name="password"
+        id="password"
+        type="password"
+        label="Password"
+        validate={[required, nonEmpty]}
+      />
+
+      {errorMessage}
+
+      <Col xs={12} className="form-button" >
+        <button
+          type="submit"
+          disabled={props.pristine || props.submitting}
+        >
+          Login
+        </button>
+      </Col>
+    </form>
+  );
 }
 
 LoginForm.propTypes = {
   pristine: PropTypes.bool,
   submitting: PropTypes.bool,
   error: PropTypes.string,
-  handleSubmit: PropTypes.func,
+  dispatch: PropTypes.func,
+  handleSubmit: PropTypes.func
 };
 
 LoginForm.defaultProps = {
   pristine: true,
   submitting: false,
-  error: '',
-  handleSubmit: () => console.log('form submitted'),
+  error: ''
 };
 
 export default reduxForm({
