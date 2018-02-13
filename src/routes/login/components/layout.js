0903d24c8b68e7f2292d773b16860d5f3a42435d
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-02-12 19:47:14 -0700

    added facebook login button

--- src/routes/login/components/layout.js
+++ src/routes/login/components/layout.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
+import FacebookLogin from 'react-facebook-login';
 import LoginForm from './login-form';
+import './layout.css';
 
 export default function Layout(props) {
   if (props.loggedIn) {
@@ -11,11 +13,26 @@ export default function Layout(props) {
     );
   }
 
+  function sendFacebookResToAPI(res) {
+    console.log('facebook response', res);
+  }
+
   return (
     <main role="main" className="login-container">
       <Col xs={12}>
         <h1 className="form-header">Login</h1>
         <LoginForm {...props} />
+      </Col>
+
+      <Col xs={12}>
+        <FacebookLogin
+          appId="335446923615924"
+          autoLoad={true}
+          fields="name,email"
+          callback={sendFacebookResToAPI}
+          cssClass="facebook-btn"
+          icon="fa-facebook"
+        />
       </Col>
     </main>
   );
