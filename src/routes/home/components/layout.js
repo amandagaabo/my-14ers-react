a41de379e77d9f7e33fe73c58db9e2435a1b22d9
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-02-15 10:59:46 -0700

    added content to home page

--- src/routes/home/components/layout.js
+++ src/routes/home/components/layout.js
@@ -12,31 +12,45 @@ export default function Layout() {
       </Jumbotron>
       <Col xs={12} className="home-content container">
         <h1>Your 14er checklist</h1>
-        <Col xs={12} md={4}>
+        <Col className="home-info" sm={10} smOffset={1} lg={3} lgOffset={1}>
           <img
             src="https://res.cloudinary.com/amhprojects/image/upload/v1518643033/14ers/hiker-icon.png"
             alt="hiker"
             className="home-icon"
           />
-          <p>Hike a Colorado 14er</p>
+          <h2>Hike a Colorado 14er</h2>
+          <p>
+            There are 58 mountains in Colorado with peaks that are at 14,000 ft or taller.
+            They are challenging hikes of varying lengths and difficulty levels. Choose one,
+            prepare for it and hike it!
+          </p>
         </Col>
 
-        <Col xs={12} md={4}>
+        <Col className="home-info" sm={10} smOffset={1} lg={4} lgOffset={0}>
           <img
             src="https://res.cloudinary.com/amhprojects/image/upload/v1518643137/14ers/checklist-icon.png"
             alt="checklist"
             className="home-icon"
           />
-          <p>Mark it as completed</p>
+          <h2>Mark it as completed</h2>
+          <p>
+            Use my14ers to keep track of the 14ers you have hiked. Add a peak to your completed list
+            after you finish hiking it. Include the date you climbed it and some notes if you want.
+            View your completed peaks in a list or on a map.
+          </p>
         </Col>
 
-        <Col xs={12} md={4}>
+        <Col className="home-info" sm={10} smOffset={1} lg={3} lgOffset={0}>
           <img
             src="https://res.cloudinary.com/amhprojects/image/upload/v1518642680/14ers/high-five-icon.svg"
             alt="high-five"
             className="home-icon"
           />
-          <p>Hike them all!</p>
+          <h2>Hike them all!</h2>
+          <p>
+            It is often a goal of people who live in Colorado to complete all the Colorado 14ers.
+            Make it your goal too and track your progress on your my14ers dashboard.
+          </p>
         </Col>
       </Col>
     </main>
