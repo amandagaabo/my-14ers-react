76b7b2794f9ee0b3a8b296d04d4cfb911159c59f
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-02-16 10:08:06 -0700

    initial card design

--- src/routes/peak-list/components/layout.js
+++ src/routes/peak-list/components/layout.js
@@ -72,13 +72,33 @@ export default class Layout extends React.Component {
               onMouseEnter={() => this.onMouseEnter(peak.uuid)}
               onMouseLeave={() => this.onMouseLeave(peak.uuid)}
             >
-              <img src={peak.imgSrc} alt={peak.peakName} className="mountain-photo" />
-              <div className="caption">
-                <h2 className="caption-header">{peak.peakName} - {peak.elevation}</h2>
-                <p className="caption-details">Rank: {peak.rank}</p>
-                <p className="caption-details">Date climbed: {peak.date}</p>
-                <br />
-                <p className="caption-details">{peak.notes}</p>
+              <Col className="inner-box">
+                <img src={peak.imgSrc} alt={peak.peakName} className="mountain-photo" />
+
+                <Col xs={12} className="caption-header">
+                  <h2>{peak.peakName}</h2>
+                </Col>
+
+                <Col xs={12} className="caption-details">
+                  <Col xs={4} className="detail-box right-border">
+                    <p className="caption-text">{peak.elevation}</p>
+                    <p className="caption-description">feet</p>
+                  </Col>
+
+                  <Col xs={3} className="detail-box">
+                    <p className="caption-text">{peak.rank}</p>
+                    <p className="caption-description">rank</p>
+                  </Col>
+
+                  <Col xs={5} className="detail-box left-border">
+                    <p className="caption-text">{peak.date}</p>
+                    <p className="caption-description">date</p>
+                  </Col>
+                </Col>
+
+                <Col xs={12} className="caption-notes">
+                  <p>{peak.notes}</p>
+                </Col>
 
                 <div className="hover-options">
                   <Link
@@ -90,10 +110,11 @@ export default class Layout extends React.Component {
                   <button
                     className={`button remove-peak ${visibility}`}
                     onClick={() => this.onDeleteClick(peak.uuid)}
-                  > x
+                  > <Icon name="times" />
                   </button>
                 </div>
-              </div>
+
+              </Col>
             </Col>
           );
         });
@@ -109,9 +130,8 @@ export default class Layout extends React.Component {
     return (
       <main role="main" className="peak-list-container">
         <div className="container">
-          <h1 className="page-header">Peak List</h1>
-
           <div className="peak-list-container" >
+
             <div className="list-sort">
               <label htmlFor="sort-by">Sort by: </label>
               <select id="sort-by" onChange={e => this.onSortChange(e)} value={this.props.sortBy}>
