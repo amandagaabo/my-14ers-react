0027fb49c45c01e54c800401b91ff7c9aefa1b9b
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-02-16 15:57:36 -0700

    styled add peak form

--- src/routes/add-peak/components/layout.js
+++ src/routes/add-peak/components/layout.js
@@ -3,6 +3,7 @@ import { Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { Redirect } from 'react-router-dom';
 import AddPeakForm from './add-peak-form';
+import './layout.css';
 
 export default function Layout(props) {
   if (!props.loggedIn) {
@@ -13,10 +14,11 @@ export default function Layout(props) {
 
   return (
     <main role="main" className="add-peak-container">
-      <Col xs={12}>
-        <h1 className="form-header">Add Peak</h1>
-        <AddPeakForm {...props} />
-      </Col>
+      <div className="add-form-container container">
+        <Col xs={12} md={6} mdOffset={3}>
+          <AddPeakForm {...props} />
+        </Col>
+      </div>
     </main>
   );
 }
