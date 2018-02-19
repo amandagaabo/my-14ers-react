98bcfb1f803938909f2875581f9efc8728c24d53
Amanda Herschleb <amanda.herschleb@gmail.com>
2018-02-19 14:29:23 -0700

    setup not-found page

---
+++ src/routes/not-found/components/layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Col } from 'react-bootstrap';
+import './layout.css';
+
+export default function Layout() {
+  return (
+    <main role="main" className="not-found-container">
+      <Col xs={12} className="container">
+        <h1>Not Found</h1>
+        <p>
+          The page you are looking for is not found. Check your url or start
+          from the <Link to="/" className="home-link">home</Link> page.
+        </p>
+      </Col>
+    </main>
+  );
+}
