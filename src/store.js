6fded619b8827a2152aae24f8b07f4190f6db3a7
Amanda Herschleb <amanda.herschleb@gmail.com>
2019-04-23 12:53:26 -0600

    setup redux devtools

--- src/store.js
+++ src/store.js
@@ -1,17 +1,23 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import thunk from 'redux-thunk';
 import appReducer from './modules/reducers';
 import { loadAuthToken } from './utils/local-storage';
 import { setAuthToken } from './modules/auth/actions';
 
+const reducer = combineReducers({
+  app: appReducer,
+  form: formReducer,
+});
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  combineReducers({
-    app: appReducer,
-    form: formReducer,
-  }),
-  applyMiddleware(thunk),
+  reducer,
+  composeEnhancers(
+    applyMiddleware(thunk)
+  )
 );
+/* eslint-enable */
 
 const authToken = loadAuthToken();
 if (authToken) {
